39138ef2ac0500645d805fca853d4e497236926a
Shivam Singh <sin2000shiv@gmail.com>
2025-02-25 12:53:30 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,82 @@
+let score = "33" // string
+let scores = 33 // number
+
+// console.log(typeof score);
+// console.log(typeof(scores));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// number + string
+
+let number = "14vbn"
+
+let valueInNumbers = Number(number);
+// console.log(typeof valueInNumbers);
+// console.log(valueInNumbers);
+
+// null
+
+let numbers = null
+
+let valueInNumberss = Number(numbers);
+// console.log(typeof valueInNumberss);
+// console.log(valueInNumberss);
+
+// undefined
+
+let unknow = undefined
+
+let valueInNumbeR = Number(unknow);
+// console.log(typeof valueInNumbeR);
+// console.log(valueInNumbeR);
+
+// node 01_basics/03_conversionOperation.js
+
+// boolen
+
+let boolens = true
+
+let valueInNumBER = Number(boolens);
+// console.log(typeof valueInNumBER);
+// console.log(valueInNumBER);
+
+// boolen
+
+let boolen = false
+
+let valueInNumbER = Number(boolen);
+// console.log(typeof valueInNumbER);
+// console.log(valueInNumbER);
+
+// string 
+
+let string = "Shivam"
+
+let valueInNumbERs = Number(string);
+// console.log(typeof valueInNumbERs);
+// console.log(valueInNumbERs);
+
+// "33" => 33
+//"33vbn" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+/*
+1 => true
+0 => false
+"" => false
+"Shivam" => true
+*/
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber); // number
+console.log(typeof stringNumber); // string
+
+
