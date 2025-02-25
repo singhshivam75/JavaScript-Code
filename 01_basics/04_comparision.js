2d8a8f0a21c6068de636b4a571f24adf71f70571
Shivam Singh <sin2000shiv@gmail.com>
2025-02-25 15:39:50 +0000

    comparison of datatypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,29 @@
+// node 01_basics/04_comparision.js
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+ 
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0); //false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+// console.log(null <= 0); //true
+// console.log(null < 0); //false
+// console.log(null === 0); // false
+// console.log(null === null); // true
+// console.log(null == undefined); // true
+
+// === strictly check
+
+// Use == when you want JavaScript to convert types before comparing.
+// Use === when you want to compare values without type conversion (recommended for strict comparison).
+
+console.log("2" == 2);
+console.log("2" === 2);
+
